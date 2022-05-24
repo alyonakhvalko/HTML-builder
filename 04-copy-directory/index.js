4605c4a14b8e2df85b259c3c09aa9bda73c1b052
Alyona Khvalko <85245607+alyonakhvalko@users.noreply.github.com>
2022-05-24 12:14:44 +0300

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const fs = require('fs');
 
+const dirPath = path.resolve(__dirname, 'files');
+const dirPathCopy = path.resolve(__dirname, 'files-copy');
+    
+
+
+
+fs.mkdir(dirPathCopy, { recursive: true }, err => {
+    if(err) { throw err; // не удалось создать папки
+} else {
+    //console.log('Папка успешно создана');}
+});
+
+        fs.readdir(dirPath, { withFileTypes: true }, (err, files) => { 
+    if(err) { throw err; 
+    //console.log(files); //выводим список папок
+    } else {
+
+        files.forEach(file => { 
+    //console.log(file)
+        fs.copyFile(path.resolve(dirPath, file.name), path.resolve(dirPathCopy, file.name), (err) => {
+        if (err) throw err
+        });
+        console.log('Файл скопирован успешно');
+      });
+   }
+ });
