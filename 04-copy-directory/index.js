3e1860e48e0fae14aa25a7bc92f6b5944708cddf
Alyona Khvalko <85245607+alyonakhvalko@users.noreply.github.com>
2022-05-25 11:27:47 +0300

    feat: correct solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,18 +3,23 @@ const fs = require('fs');
 
 const dirPath = path.resolve(__dirname, 'files');
 const dirPathCopy = path.resolve(__dirname, 'files-copy');
-    
 
-
-
+//создали папку 
 fs.mkdir(dirPathCopy, { recursive: true }, err => {
-    if(err) { throw err; // не удалось создать папки
-} else {
-    //console.log('Папка успешно создана');
+    if(err) { throw err; 
+    } else {
+    //console.log('Папка успешно создана'); 
 }
 });
 
-        fs.readdir(dirPath, { withFileTypes: true }, (err, files) => { 
+//читаем и удаляем содержимое(если есть)
+fs.readdir(dirPathCopy, { withFileTypes: true }, (err, files) => { 
+    if(err) { throw err; 
+    } else {
+    //console.log(files);
+}});
+
+fs.readdir(dirPath, { withFileTypes: true }, (err, files) => { 
     if(err) { throw err; 
     //console.log(files); //выводим список папок
     } else {
@@ -24,7 +29,7 @@ fs.mkdir(dirPathCopy, { recursive: true }, err => {
         fs.copyFile(path.resolve(dirPath, file.name), path.resolve(dirPathCopy, file.name), (err) => {
         if (err) throw err
         });
-        console.log('Файл скопирован успешно');
+        //console.log('Файл скопирован успешно');
       });
-   }
- });
+    }
+    });
