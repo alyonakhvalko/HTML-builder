86c8857db288524518144e19974ee729c1d028c2
Alyona Khvalko <alyonakazimirchick@gmail.com>
2022-11-03 16:22:19 +0300

    fix: correct text in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,13 +6,13 @@ const { stdin, stdout } = process;
 let pathWay = path.resolve(__dirname, 'text.txt'); //абсолютный путь к файлу на основе относительного пути к нему
 
 const output = fs.createWriteStream(pathWay); //создали текстовый документ в папке 02-write-file
-console.log('Напиши свой текст:');
+console.log('Привет! Напиши свой текст:');
 
 stdin.on('data', data => {
     const writeData = data.toString();
     if (writeData.startsWith('exit')) {
         output.close();
-        console.log('Текст записан в файл! Завершено удачно (при вводе exit)!')
+        console.log('Ваш текст записан в файл! Завершено удачно (при вводе exit)!')
         process.exit();
     } else {
         output.write(writeData);
@@ -21,6 +21,6 @@ stdin.on('data', data => {
 
 process.on('SIGINT', () => {
     output.close();
-    console.log('Текст записан в файл! Завершено удачно (при нажатии ctrl+c)!')
+    console.log('Ваш текст записан в файл! Завершено удачно (при нажатии ctrl+c)!')
     process.exit();
 });
